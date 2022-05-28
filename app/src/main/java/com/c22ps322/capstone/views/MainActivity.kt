9a964dfbf9e41bb779b2f221f9865e07bed42c70
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2022-05-28 20:27:00 +0700

    Revert "Home profile"

--- app/src/main/java/com/c22ps322/capstone/views/MainActivity.kt
+++ app/src/main/java/com/c22ps322/capstone/views/MainActivity.kt
@@ -1,46 +1,12 @@
 package com.c22ps322.capstone.views
 
-import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import android.os.Bundle
 import com.c22ps322.capstone.R
-import com.c22ps322.capstone.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity(), NavigationBarView.OnItemReselectedListener {
-
-    private lateinit var binding: ActivityMainBinding
-
-    private lateinit var navController: NavController
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
-
-        setupNavigation()
-    }
-
-
-    private fun setupNavigation(){
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
-        navController = navHostFragment.navController
-
-        binding.bottomNavBar.apply {
-            setupWithNavController(navController)
-
-            setOnItemReselectedListener(this@MainActivity)
-        }
-    }
-
-    override fun onNavigationItemReselected(item: MenuItem) {
-        navController.popBackStack(item.itemId, inclusive = false)
+        setContentView(R.layout.activity_main)
     }
 }
