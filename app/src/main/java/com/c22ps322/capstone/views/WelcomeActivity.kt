893af48daa5ae2da1794cafddc4fc6436a4e342c
robertsetiawan <72391955+robertsetiawan@users.noreply.github.com>
2022-05-29 08:52:04 +0700

    permission check in welcome activity

--- app/src/main/java/com/c22ps322/capstone/views/WelcomeActivity.kt
+++ app/src/main/java/com/c22ps322/capstone/views/WelcomeActivity.kt
@@ -1,14 +1,56 @@
 package com.c22ps322.capstone.views
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.c22ps322.capstone.R
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            requestCameraPermission()
+        }
+    }
+
+    private fun requestCameraPermission(){
+
+        if (isCameraPermissionGranted()) startCamera() else requestPermission.launch(
+            REQUIRED_CAMERA_PERMISSION)
+
+    }
+
+    private fun startCamera() {
+        val intent = Intent(this, MainActivity::class.java)
+
+        startActivity(intent)
+    }
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { permission ->
+            if (permission == true) {
+                startCamera()
+
+            } else {
+
+                Toast.makeText(applicationContext, "Permission tidak diberikan", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private fun isCameraPermissionGranted() = ContextCompat.checkSelfPermission(this, REQUIRED_CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED
+
+
+    companion object {
+        private const val REQUIRED_CAMERA_PERMISSION = Manifest.permission.CAMERA
     }
 }
