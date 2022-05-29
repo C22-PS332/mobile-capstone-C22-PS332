204b45e31eac1a617eff7e504e18e62a8b048495
robertsetiawan <72391955+robertsetiawan@users.noreply.github.com>
2022-05-29 08:52:56 +0700

    home section ui finished

--- app/src/main/java/com/c22ps322/capstone/views/home/ResultSheetFragment.kt
+++ app/src/main/java/com/c22ps322/capstone/views/home/ResultSheetFragment.kt
@@ -1,4 +1,90 @@
 package com.c22ps322.capstone.views.home
 
-class ResultSheetFragment {
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.c22ps322.capstone.databinding.FragmentResultSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.R
+
+class ResultSheetFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentResultSheetBinding? = null
+
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentResultSheetBinding.inflate(inflater)
+
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.ivClose?.setOnClickListener { this.dismiss() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val view: FrameLayout = dialog?.findViewById(R.id.design_bottom_sheet)!!
+
+        view.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+
+        val behavior = BottomSheetBehavior.from(view)
+
+        behavior.peekHeight = 3000
+
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+
+    override fun onCancel(dialog: DialogInterface) {
+        dialog.dismiss()
+        super.onCancel(dialog)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            ResultSheetFragment()
+    }
 }
