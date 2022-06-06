248ee1e3cc0763a7c3528e98558f2e13f71ed86e
robertsetiawan <72391955+robertsetiawan@users.noreply.github.com>
2022-06-07 00:28:00 +0700

    Setup spoonacular food module

--- app/src/main/java/com/c22ps322/capstone/modules/network/FoodModule.kt
+++ app/src/main/java/com/c22ps322/capstone/modules/network/FoodModule.kt
@@ -2,6 +2,7 @@ package com.c22ps322.capstone.modules.network
 
 import com.c22ps322.capstone.modules.DomainRetrofit
 import com.c22ps322.capstone.modules.EdamamRetrofit
+import com.c22ps322.capstone.modules.SpoonacularRetrofit
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -24,4 +25,10 @@ object FoodModule {
     @Provides
     fun provideEdamamService(@EdamamRetrofit retrofit: Retrofit.Builder): EdamamFoodService =
         retrofit.build().create(EdamamFoodService::class.java)
+
+
+    @Singleton
+    @Provides
+    fun provideSpoonacularService(@SpoonacularRetrofit retrofit: Retrofit.Builder): SpoonacularFoodService =
+        retrofit.build().create(SpoonacularFoodService::class.java)
 }
