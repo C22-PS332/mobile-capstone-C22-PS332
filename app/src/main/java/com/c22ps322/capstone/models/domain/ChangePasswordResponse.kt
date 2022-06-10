0e98499f9fcf95b85b27991f535235f76f886acf
robertsetiawan <72391955+robertsetiawan@users.noreply.github.com>
2022-06-10 22:01:16 +0700

    Update change password model

--- app/src/main/java/com/c22ps322/capstone/models/domain/ChangePasswordResponse.kt
+++ app/src/main/java/com/c22ps322/capstone/models/domain/ChangePasswordResponse.kt
@@ -4,7 +4,5 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ChangePasswordResponse(
-    val message: String?,
-
-    val detail: String?
+    val message: String?
 )
