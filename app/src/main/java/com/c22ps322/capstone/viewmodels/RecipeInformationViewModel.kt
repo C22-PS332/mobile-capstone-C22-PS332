7fee1ebecbabefbc44f94345b0de0ebde262377a
robertsetiawan <72391955+robertsetiawan@users.noreply.github.com>
2022-06-11 20:03:06 +0700

    Change concrete dependency to abstraction

--- app/src/main/java/com/c22ps322/capstone/viewmodels/RecipeInformationViewModel.kt
+++ app/src/main/java/com/c22ps322/capstone/viewmodels/RecipeInformationViewModel.kt
@@ -1,13 +1,13 @@
 package com.c22ps322.capstone.viewmodels
 
 import androidx.lifecycle.ViewModel
-import com.c22ps322.capstone.repositories.FoodRepository
+import com.c22ps322.capstone.repositories.AbstractFoodRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipeInformationViewModel @Inject constructor(
-    private val foodRepository: FoodRepository
+    private val foodRepository: AbstractFoodRepository
 ): ViewModel() {
 
     suspend fun getRecipeInformation(id: Int) = foodRepository.getRecipeInformation(id)
