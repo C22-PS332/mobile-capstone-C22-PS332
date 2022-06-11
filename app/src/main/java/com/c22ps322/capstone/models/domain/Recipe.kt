1cd589dc56d2e245790166e9d6aa49dbaa2b2e24
robertsetiawan <72391955+robertsetiawan@users.noreply.github.com>
2022-06-11 14:45:36 +0700

    Change Dummy recipe to Recipe

--- app/src/main/java/com/c22ps322/capstone/models/domain/Recipe.kt
+++ app/src/main/java/com/c22ps322/capstone/models/domain/Recipe.kt
@@ -1,17 +1,19 @@
 package com.c22ps322.capstone.models.domain
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
+@JsonClass(generateAdapter = true)
 @Parcelize
-data class DummyRecipe(
+data class Recipe(
     val id: Int,
 
     val title: String,
 
-    val desc: String,
+    val summary: String,
 
+    @Json(name = "image")
     val imageUrl: String,
-
-    val ingredients: ArrayList<String>
 ): Parcelable
