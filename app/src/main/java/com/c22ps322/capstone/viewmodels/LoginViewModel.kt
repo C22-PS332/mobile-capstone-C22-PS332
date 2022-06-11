6fd2679943e6076897ff06b3960f2c916f1b048f
robertsetiawan <72391955+robertsetiawan@users.noreply.github.com>
2022-06-12 00:05:59 +0700

    Remove unused imports

--- app/src/main/java/com/c22ps322/capstone/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/c22ps322/capstone/viewmodels/LoginViewModel.kt
@@ -1,7 +1,6 @@
 package com.c22ps322.capstone.viewmodels
 
 import androidx.lifecycle.ViewModel
-import com.c22ps322.capstone.databinding.FragmentChangePasswordBinding
 import com.c22ps322.capstone.repositories.AbstractUserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
